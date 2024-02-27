b89f7f9c9520d00c81c37a05348ded7cd8c80e2a
Olsson <borozytoja@gmail.com>
2024-02-27 00:42:57 +0000

    Update build.sql

--- build.sql
+++ build.sql
@@ -1,1 +1,18 @@
+create database chicagobulls
+
+CREATE TABLE 
+    `chicagobulls`.players 
+    ( 
+        Name        TEXT, 
+        Surname     TEXT, 
+        Nationality TEXT, 
+        Age         INT 
+    ) 
+    ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+
+INSERT INTO players (Name, Surname, Nationality, Age) VALUES (Zach, LaVine, American, 28);
+INSERT INTO players (Name, Surname, Nationality, Age) VALUES (DeMar, DeRozan, American, 34);
+INSERT INTO players (Name, Surname, Nationality, Age) VALUES (Alec, Jacoby, American, 24);
+INSERT INTO players (Name, Surname, Nationality, Age) VALUES (Lonzo, Ball, American, 26);
+INSERT INTO players (Name, Surname, Nationality, Age) VALUES (Nikola, Vucevic, Switzerland, 33);
 
